ecb6fcb0f9698566ee85abd6f109f8f083bd7546
nekit155rybin <nekit155rybin@gmail.com>
2021-05-31 23:57:49 +0300

    Add project files.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+ 
+#include <iostream>
+// Вызывает функцию
+void print() {
+
+    //Потому что хотим исаользовать стандартную библиотку в консоль и 
+    //не использовать какие либо другие библиотеки.
+    std::cout << "Hellow Skillbox!\n";
+    /*
+    присваеваем зеачение пременной и печаетем hellow world
+
+    */
+
+}
+int main()
+{
+    int x = 100;
+    int y = x + 100;
+    int mult = x * y;
+    int b = 0;
+    b = b + 2;
+    int test;
+    test = 100500;
+    int test2 = 1005001;
+    int rundom = 1002;
+
+    std::cout << rundom;
+   
+}
+
+ 
